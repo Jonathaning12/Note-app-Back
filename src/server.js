ae51b66ff450da4cc00a6f3868eb16b87cbdd78e
Usuario <usuario@example.com>
2026-05-14 21:59:50 -0600

    actualizacion de cors

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ import{connectDB} from "./config/DB.js"
 import cors from "cors"
 dotenv.config()
 const app= express()
-app.use(cors({origin: "http://localhost:5173"})) // Habilitar CORS para todas las rutas
+app.use(cors({origin: ["http://localhost:5173", "https://bright-babka-132835.netlify.app/"]})) // Habilitar CORS para todas las rutas
 app.use(express.json()) // Middleware para parsear JSON en las solicitudes
 app.use("/api/notas", notesRouter)
-
+//probandi el status git
 const PORT=process.env.PORT || 3000
-
+console.log("solo para actuqlizar el status del git")
 console.log(process.env.PORT)
 connectDB()
 .then(() => {
